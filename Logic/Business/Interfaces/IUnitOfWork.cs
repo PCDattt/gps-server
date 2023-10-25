e063d883639085ae1db9a120cfa4ea00c562a888
PCDattt <datkg1097@gmail.com>
2023-10-25 15:31:01 +0700

    Add UnitOfWork

---
+++ Logic/Business/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gps_server.Logic.Business.Interfaces
+{
+	public interface IUnitOfWork
+	{
+		IUserRepository UserRepository { get; set; }
+		IDeviceRepository DeviceRepository { get; set; }
+		IDevicePacketRepository DevicePacketRepository { get; set; }
+	}
+}
