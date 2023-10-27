2357c49f87fc7d62962622a2bb6cea576eb45138
PCDattt <datkg1097@gmail.com>
2023-10-27 11:14:07 +0700

    Convert console app to WebApi

--- Program.cs
+++ Program.cs
@@ -1,21 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO.Hashing;
-using gps_server.Data.Entity.Entities;
-using gps_server;
-using gps_server.Logic.Business.Repositories;
+﻿//// See https://aka.ms/new-console-template for more information
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.IO.Hashing;
+//using gps_server.Data.Entity.Entities;
+//using gps_server;
+//using gps_server.Logic.Business.Repositories;
 
-namespace gps_server
+//namespace gps_server
+//{
+//	public class Program
+//	{
+//		public static void Main()
+//		{
+
+//		}
+//	}
+//}
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-	public class Program
-	{
-		public static async Task Main()
-		{
-			await TcpServer.Run();
-		}
-	}
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
